9406b2dc343a4f757bfcaa71f6cacd22093a2445
PsychoKramps <Fraiden70@gmail.com>
2022-09-16 11:22:38 -0500

    Primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumesComponent } from './albumes/albumes.component';
+import { HistoriaComponent } from './historia/historia.component';
+import { HomeComponent } from './home/home.component';
+import { IntegrantesComponent } from './integrantes/integrantes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'historia',
+    component:HistoriaComponent
+  },
+  {
+    path:'integrantes',
+    component:IntegrantesComponent
+  },
+  {
+    path:'albumes',
+    component:AlbumesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { HistoriaComponent } from './historia/historia.component';
+import { IntegrantesComponent } from './integrantes/integrantes.component';
+import { AlbumesComponent } from './albumes/albumes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent,
+    HistoriaComponent,
+    IntegrantesComponent,
+    AlbumesComponent
   ],
   imports: [
     BrowserModule,
