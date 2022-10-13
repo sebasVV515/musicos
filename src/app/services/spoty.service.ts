3047e79f12a1aac8aa9d231dc5eff17b18268cbd
308 <308@academicos.cesde.edu.co>
2022-10-13 09:14:21 -0500

    arreglo token

--- src/app/services/spoty.service.ts
+++ src/app/services/spoty.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class SpotyService {
   URI:string='https://api.spotify.com/v1/artists/5eAWCfyUhZtHHtBdNk56l1/top-tracks?market=US'
-  TOKEN:string='Bearer BQBBOBj-RHpvDevA3bz4OGlKtVBs1vElWvXgrn1s5trmEPqPGOhSFfikIhE2d8NwhQd6dsIMX9XN0TXotj58LFGG6rrG2vGneQ5279094Yvuhz40BSqx3udZVClajzZCJQKOaWmk5lPYdB8WTfXIU_EA6PIZFo1BCam3q1NvymxfXmN7N_FZdTdiE7MEuENQfK0'
+  TOKEN:string='Bearer BQC6WFIj6GkQKi5aCWI6jAolEiz5PzJt4vRMiNyDPa1lbZGpVAyd0YqILIktl0PE3Ta9V-d2JMwQyZxuTDqklukatBoc27wHFZfMkc6skiX6wtLyYKmIxC6TRnYaujLXW4S-LIdJv35yZktTUSoIhy5ykYnBMQI0y013qVchcK2Occuty_htgbBeY-j1hHSZ3Iw'
   constructor(public peticion:HttpClient){
     console.log("Start Spoty API")
   }
